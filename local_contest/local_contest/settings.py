046cbf55ff0cff597c7ff058ddf48267fdbd020a
Tomixbo <tomixbo@gmail.com>
2024-12-20 10:26:27 +0300

    fix debug settings

--- local_contest/local_contest/settings.py
+++ local_contest/local_contest/settings.py
@@ -30,7 +30,7 @@ MEDIA_URL = '/media/'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 't')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
